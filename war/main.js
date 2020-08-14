dda1b1b11e2b29b8b551d4491a8c721e5a5d486b
Zinnig <38312982+Zinnig@users.noreply.github.com>
2020-08-14 06:59:01 +0200

    Fixed Snowflake to date

--- war/main.js
+++ war/main.js
@@ -1,12 +1,12 @@
 function getInput(){
     let input = parseInt(document.getElementById('input').value)
     let output = document.getElementById('output');
-    let inputBNum = input.toString(2)
-    let inputMS = inputBNum.slice(0, 36)
-    let inputMSInt = parseInt(inputMS, 2)
-    let inputTime = inputMSInt
+    inputBNum = input.toString(2)
+    inputMS = inputBNum.slice(0, 38)
+    inputMSInt = parseInt(inputMS, 2)
+    inputTime = inputMSInt
     inputTime += 1420070400000
-    let inputTimeStamp = new Date(inputTime);
+    inputTimeStamp = new Date(inputTime);
     output.innerHTML = inputTimeStamp.toUTCString()
     
 
